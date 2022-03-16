3d4f8826fd53248d3f8aad4d7c120f399968b8a7
DESKTOP-0K5Q27J\uinhi <datltth2009017@fpt.edu.vn>
2022-03-16 10:54:12 +0700

    dattestgit

---
+++ CourseOnline/Areas/Admin/Controllers/DattestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Areas.Admin.Controllers
+{
+    public class DattestController : Controller
+    {
+        // GET: Admin/Dattest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
