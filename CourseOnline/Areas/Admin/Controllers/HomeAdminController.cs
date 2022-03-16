29c52abc860d136523f66982cfd5711e3a4cbb08
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-03-16 21:47:01 +0700

    temblate admin

---
+++ CourseOnline/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Areas.Admin.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: Admin/HomeAdmin
+        public ActionResult Index()
+        {
+            return View();
+        }// GET: Admin/Dattest
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Widgets()
+        {
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult FormWizard()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterAdmin()
+        {
+            return View();
+        }
+    }
+}
