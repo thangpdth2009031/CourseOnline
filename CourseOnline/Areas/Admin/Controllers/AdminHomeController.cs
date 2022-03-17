1777423a462a82c8796ad4f15cd76122c9a59e13
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-03-17 10:32:23 +0700

    testgit

---
+++ CourseOnline/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: Admin/Dattest
+        public ActionResult Index()
+        {
+            return View();
+        }// GET: Admin/Dattest
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Charts()
+        {
+            return View();
+        }
+
+        public ActionResult Widgets()
+        {
+            return View();
+        }
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+    }
+}
