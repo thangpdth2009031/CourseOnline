be936ed73f1d2ba2f866e07be9a77efbf114c2f2
haduynhat2002 <nhathdth2009032@fpt.edu.vn>
2022-03-18 09:07:37 +0700

    Teamblate accoutn

--- CourseOnline/Controllers/HomeController.cs
+++ CourseOnline/Controllers/HomeController.cs
@@ -36,7 +36,17 @@ namespace CourseOnline.Controllers
         {
             return View();
         }
-        
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
