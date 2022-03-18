6095b8a787da714a95043cff603d329bc654fab7
DESKTOP-0K5Q27J\uinhi <datltth2009017@fpt.edu.vn>
2022-03-18 16:50:51 +0700

    commit chi tiet khoa hoc

---
+++ CourseOnline/Controllers/CouseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseOnline.Controllers
+{
+    public class CouseController : Controller
+    {
+        // GET: Couse
+        public ActionResult CouseDetail()
+        {
+            return View();
+        }
+    }
+}
