6714d52335ee7213ddc1854e40f7f25cfcabcc8b
thangpdth2009031@fpt.edu.vn <thangpdth2009031@fpt.edu.vn>
2022-03-19 08:54:19 +0700

    create-user

---
+++ CourseOnline/Areas/Admin/Core.Utils/VariableExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseOnline.Areas.Admin.Core.Utils
+{
+    public class VariableExtension
+    {
+    }
+    public class SelectListModel
+    {
+        public object Key { get; set; }
+        public string Value { get; set; }
+    }
+    public static class GenderKey
+    {
+        public const int Male = 1;
+        public const int FeMale = 0;
+
+        public static List<SelectListModel> GetGender()
+        {
+            return new List<SelectListModel>() {
+                new SelectListModel{Key=1,Value="Nam" },
+                new SelectListModel{Key=0,Value="Nữ" }
+            };
+        }
+
+        public static string GetEmployeeType(int type)
+        {
+            switch (type)
+            {
+                case 0:
+                    return "Nữ";
+
+                case 1:
+                    return "Nam";
+
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
