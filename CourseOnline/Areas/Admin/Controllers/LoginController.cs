517a0e3a15ef88fb143f51ff379eee0fad0e94ac
tiendvth <tiendvth2009015@fpt.edu.vn>
2022-03-22 03:48:18 +0700

    login

---
+++ CourseOnline/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using CourseOnline.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using CourseOnline.Areas.Admin.Models;
+using CourseOnline.Dao;
+using CourseOnline.Models;
+
+namespace CourseOnline.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.Username, Encryptor.MD5Hash(model.Password));
+                if (result == 1)
+                {
+                    var user = dao.GetById(model.Username);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserId = user.Id;
+                   HttpContext.Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "HomeAdmin");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không đúng");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản bị khóa");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Mật khẩu không đúng");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tên đặng nhập hoặc mật khẩu không đúng!");
+
+                }
+            }
+        
+        
+            return View("Index");
+        }
+
+
+    }
+}
---
+++ CourseOnline/Common/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CourseOnline.Common
+{
+    public class UserLogin
+    {
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+       
+    }
+}                                                                                                                                                                                                                                                                                                                                                                            
