1e718d4bb0426e4c57fa58c2ed0bc4f21b4a98bc
thangpdth2009031@fpt.edu.vn <thangpdth2009031@fpt.edu.vn>
2022-03-30 16:36:39 +0700

    search course

--- CourseOnline/Dao/CourseDao.cs
+++ CourseOnline/Dao/CourseDao.cs
@@ -1,4 +1,5 @@
 ﻿using CourseOnline.Models;
+using CourseOnline.Models.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,11 @@ namespace CourseOnline.Dao
         public List<Category> ListAllCate()
         {
             return db.Categories.Where(x => x.Status == true).OrderBy(x => x.Id).ToList();
-        }    
+        }
+        public List<Cours> Search(string keyword)
+        {
+            var certificate = db.Courses.Where(s => s.CourseName.Contains(keyword));
+            return certificate.ToList();
+        }
     }
 }
